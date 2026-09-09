ab05535d317cdcaa033dced1929a96f4c6387a91
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-09 23:32:45 +0500

    Task1

---
+++ Tyuiu.SadykovSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Савелий";
+int age = 17;
+Console.WriteLine("Здраствуйте меня зовут "+name+" мне "+age+" лет!");
