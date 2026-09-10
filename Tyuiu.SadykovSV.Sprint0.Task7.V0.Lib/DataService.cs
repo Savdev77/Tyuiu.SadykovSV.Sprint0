8d5277cc051439e9b7d4cc2839cc93c680741d30
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-10 15:06:59 +0500

    Task7

---
+++ Tyuiu.SadykovSV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.SadykovSV.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] nums1, int[] nums2)
+        {
+            int[] result = new int[nums1.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = nums1[i] + nums2[i];
+            }
+            return result;
+        }
+    }
+}
