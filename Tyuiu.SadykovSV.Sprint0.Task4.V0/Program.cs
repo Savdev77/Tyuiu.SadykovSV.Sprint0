162d676ca6120e467b2bb842f2c6d038d1f32887
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-10 12:08:45 +0500

    Task4

---
+++ Tyuiu.SadykovSV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.SadykovSV.Sprint0.Task4.V0.Lib;
+Console.WriteLine(DataService.Addition(1,5));
+Console.WriteLine(DataService.Multiplication(1,5));
+Console.WriteLine(DataService.Subtraction(1,5));
+Console.WriteLine(DataService.Division(1,5));
+Console.ReadKey();
