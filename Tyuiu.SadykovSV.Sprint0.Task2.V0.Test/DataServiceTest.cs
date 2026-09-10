6b62ffb543a5704eb73f33cc260090699328b6fb
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-10 11:26:15 +0500

    Task2

---
+++ Tyuiu.SadykovSV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SadykovSV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SadykovSV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChenkGetMessageValid()
+        {
+            var name = "Савелий";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Савелий", res);
+        }
+    }
+}
---
+++ Tyuiu.SadykovSV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.SadykovSV.Sprint0.Task2.V0.Lib;
+Console.WriteLine(DataService.GetMessage("Савелий"));
+Console.ReadKey();
+Console.ReadKey();
