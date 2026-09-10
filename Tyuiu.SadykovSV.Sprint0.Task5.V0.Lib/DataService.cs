0ddf881ccd6d3b80dcceab8b291e445cf0c4906a
ssssadykov86@gmail.com <ssssadykov86@gmail.com>
2026-09-10 13:15:13 +0500

    Task5

---
+++ Tyuiu.SadykovSV.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.SadykovSV.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                return 0;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Tyuiu.SadykovSV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.SadykovSV.Sprint0.Task5.V0.Lib;
+Console.WriteLine("A+B = " + DataService.Addition(5, 5));
+Console.WriteLine("A-B = " + DataService.Subtraction(10, 5));
+Console.WriteLine("A*B = " + DataService.Multiplication(10, 5));
+Console.WriteLine("A/B = " + DataService.Division(9, 3));
+Console.WriteLine("A/B = " + DataService.Division(9, 0));
+Console.ReadKey();
